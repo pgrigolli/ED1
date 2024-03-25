2480fee0f7cebabb823c88a04a71842d6431bf5f
Pedro Grigolli <pedrogrigolli30@gmail.com>
2024-03-25 20:31:30 -0300

    Add VSCode configuration files and new C++ source files

---
+++ AulaPonteiros.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+
+
+
+int main(){
+
+    int a = 0;
+    for(int i = 0; i > -1; i++){
+        printf("%d ", a);
+        a++;
+    }
+
+
+
+
+}
---
+++ Ex03.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int stringFindFirst(string s, char c){
+    int idx = 0;
+
+    for(char k: s){
+    if(k == c) return idx;
+    idx++;
+
+    }
+    return -1;
+}
+
+bool isLetter(char c){
+
+    string allLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for(char j: allLetters){
+        if(c == j) return true;
+    }
+    return false;
+}
+
+
+bool checkName(string name){
+
+    if(name[0] == ' ' || name[name.size() - 1] == ' ') return false;
+
+
+
+    for(int i = 1; i < name.size(); i++){
+        if(!isLetter(name[i]) && name[i] != ' '){
+            return false;
+        } 
+
+        if(name[i] == ' ' && name[i - 1] == ' '){
+            return false;
+        }
+
+    }
+
+    return true;
+
+
+
+}
+
+int* createArray(int n, bool asc){
+
+    int* vet2 = (int*)calloc(n, sizeof(int));
+    int tam = n;
+
+    if(asc == 1){
+        for(int i = 1; i <= n; i++){
+            vet2[i] = i;
+
+        }
+    }else{
+        for(int i = 0; i < n; i++){
+            vet2[i] = tam;
+            tam--;
+            
+        }
+    }
+
+    // for(int i = 0; i < n; i++){
+    //     printf("%d ", vet2[i]);
+    // }
+
+
+    return vet2;
+
+
+}
+
+
+int* arrayConcat(int *v1, int n1, int *v2, int n2){
+
+    int* v3 = (int*)calloc(n1 + n2, sizeof(int));
+
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+    // cout << isLetter('h') << endl;
+
+    // cout << checkName("Chuck KickNorris") << endl;
+
+    createArray(10, 0);
+
+
+}
